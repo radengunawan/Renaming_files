29c50f8139c6f600346307bb03d8c7f94a5e410e
Raden Darajat Bambang Eka Gunawan <send.raden@gmail.com>
2022-05-04 09:42:44 +0800

    FINAL

--- Rename all imagefiles/Rename_all_imagefiles.py
+++ Rename all imagefiles/Rename_all_imagefiles.py
@@ -9,21 +9,15 @@ import matplotlib.pyplot as plt
 from datetime import date
 from tqdm import tqdm
 
+print(os.getcwd())
+
 today = date.today()
 
 # dd/mm/YY
 d1 = today.strftime("%d%m")
 
 folder_jpg = 'D:\\Raw_Data\\data_boxes_with_pallet\\pallet_video\\'
-
 list_of_jpg  = [f for f in os.listdir(folder_jpg) if f.endswith('.jpg')] 
-#list_of_mp4 = [f for f in os.listdir(folder_jpg) if f.endswith('.mp4')]
-
-
-#a = folder_jpg + list_of_jpg[0]
-#b = folder_jpg + list_of_mp4[0]
-#print(a)
-#print(b)
 
 
 i = 0
@@ -37,12 +31,11 @@ for item in tqdm(list_of_jpg):
     elif (i > 100):
         prefix = ""
     old_name = folder_jpg+item
-    new_name = folder_jpg+"pallete_"+prefix+str(i)+"_"+d1+".jpg"
+    new_name = folder_jpg + "ImgPallete_" + d1 + "_" +  prefix+str(i)+".jpg"
 
     os.rename(old_name, new_name)
     i +=1
 
-print("Finished!!")
-
+print("\nFinished!!")
 
 
